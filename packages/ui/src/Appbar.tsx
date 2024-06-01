aa65b9c5b5015ed5ec878919d6271faab0596152
Sasidhar-Sunkesula <sasidharsunkesula579@gmail.com>
2024-06-01 16:51:32 +0530

    Minor ui bits

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -19,7 +19,7 @@ export const Appbar = ({ user, onSignin, onSignout, headerLinks }: AppbarProps) 
   return (
     <div className="flex justify-between border-b py-2 px-4 border-slate-300">
       <div className="text-xl font-bold text-red-900  flex items-center justify-center">No Hunger</div>
-      <div className="flex items-center border gap-x-5 border-red-600 justify-center pt-2">
+      <div className="flex items-center border gap-x-5 justify-center pt-2">
         {headerLinks.map((item: LinkProps, index: number) => (
           <LinkComponent key={index} item={item} />
         ))}
