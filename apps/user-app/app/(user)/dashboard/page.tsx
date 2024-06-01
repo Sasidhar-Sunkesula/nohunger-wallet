b74b75a8bb22fa8d507753e4c351032c27248c50
Sasidhar-Sunkesula <sasidharsunkesula579@gmail.com>
2024-06-01 16:45:30 +0530

    Added recent orders

--- apps/user-app/app/(user)/dashboard/page.tsx
+++ apps/user-app/app/(user)/dashboard/page.tsx
@@ -1,15 +1,32 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../../lib/auth";
+import { getBalance } from "@/app/lib/actions/getTransactions";
+import { BalanceCard } from "@/components/BalanceCard";
+import { OnRampTransactions } from "@/components/OnRampTransactions";
+import prisma from "@repo/db/client";
 export default async function () {
   const session = await getServerSession(authOptions);
   if (!session?.user) {
     redirect("/api/auth/signin");
   }
+  const balance = await getBalance();
+  const transactionsArr = await prisma.orders.findMany({
+    where: {
+      userId: Number(session.user.id),
+    },
+  });
+  const transactions = transactionsArr.map((item) => ({
+    time: item.date,
+    amount: item.orderTotal,
+    status: item.status,
+    provider: "",
+  }));
   return (
-    <div className="flex flex-col gap-3 p-16">
+    <div className="flex flex-col gap-3 w-max mx-auto p-16">
       <div className="text-3xl font-bold mt-6">
-        Welcome to the Dashboard {session.user.email}
+        Welcome to your Dashboard{" "}
+        <span className="text-blue-600">{session.user.name}</span>
       </div>
       <div className="text-xl font-normal">
         Add money into your wallet 👜 and avail exclusive offers and cashbacks
@@ -17,6 +34,14 @@ export default async function () {
       </div>
       <div className="text-xl font-normal">Supports Multiple banks</div>
       <div className="text-xl font-normal">Secure transactions</div>
+      <BalanceCard amount={balance.amount} locked={balance.locked} />
+      <div className="pt-4">
+        <OnRampTransactions
+          title="Recent orders"
+          transactions={transactions}
+          plusMinus="-"
+        />
+      </div>
     </div>
   );
 }
