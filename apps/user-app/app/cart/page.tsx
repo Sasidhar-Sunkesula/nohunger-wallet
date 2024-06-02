e9a629d81a8f8ba90ee1b1b1499f0b79f100fd3c
Sasidhar-Sunkesula <sasidharsunkesula579@gmail.com>
2024-06-02 21:31:18 +0530

    Added wallet balance display in cart page

--- apps/user-app/app/cart/page.tsx
+++ apps/user-app/app/cart/page.tsx
@@ -13,12 +13,15 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import Spinner from "@repo/ui/spinner";
 import { makePayment } from "../lib/actions/makePayment";
+import { getBalance } from "../lib/actions/getTransactions";
+import Link from "next/link";
 
 export default function Cart() {
   const [loading, setLoading] = useState(true);
   const [fetchingPrice, setFetchingPrice] = useState(true);
   const [isProcessing, setIsProcessing] = useState(false);
   const itemList = useSelector((store: RootState) => store.cart.items);
+  const [balance, setBalance] = useState(-1);
   const dispatchFun = useDispatch();
   const session = useSession();
   const [cost, setCost] = useState(-1);
@@ -32,6 +35,13 @@ export default function Cart() {
     setLoading(false);
   };
 
+  const fetchBalance = async () => {
+    const data = await getBalance();
+    if (!data.amount) {
+      return;
+    }
+    setBalance(data.amount);
+  };
   const handleClearCart = async () => {
     dispatchFun(clearCart());
     await clearCartDb(parseInt(session.data?.user.id!));
@@ -46,13 +56,13 @@ export default function Cart() {
     if (session.data?.user) {
       const userId = session.data?.user.id;
       fetchCart(parseInt(userId));
+      fetchBalance();
     }
     // So, include session.data?.user in the dependency array of useEffect
   }, [session.data?.user]);
 
   useEffect(() => {
     const fetchTotal = async (userId: number) => {
-      setFetchingPrice(true);
       const total = await getCartTotal(userId);
       if (!total.cost) {
         console.log(total.error);
@@ -72,10 +82,17 @@ export default function Cart() {
       <div className="text-xl w-4/5 text-center mt-5 mb-3 mx-auto font-bold">
         Cart - {itemList.length} items
       </div>
-      <div className="w-4/5 text-right mx-auto">
+      <div className="w-4/5 flex justify-between mb-5 items-center mx-auto">
+        <Link
+          className="bg-green-100 text-green-800 text-base font-medium me-2 p-2 rounded dark:bg-green-900 dark:text-green-300"
+          href={"/dashboard"}
+        >
+          {balance !== -1 && `Available wallet balance : ₹${balance}`}
+        </Link>
         <button
           onClick={() => handleClearCart()}
-          className=" bg-red-500 p-2  font-medium text-sm rounded-lg text-white"
+          type="button"
+          className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
         >
           Clear Cart
         </button>
@@ -83,7 +100,7 @@ export default function Cart() {
       {loading ? (
         <Spinner />
       ) : (
-        <div>
+        <div className="px-5">
           {itemList.map((item: Item, index: number) => (
             <div className="flex justify-center items-center" key={item.id}>
               <ItemList
@@ -93,8 +110,12 @@ export default function Cart() {
                 ratings={item.ratings}
                 imageId={item.imageId}
               />
-              <button onClick={() => handleDelete(index, item)}>
-                <img src="deleteIcon.svg" alt="delete" />
+              <button
+                onClick={() => handleDelete(index, item)}
+                type="button"
+                className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+              >
+                Delete
               </button>
             </div>
           ))}
@@ -103,13 +124,13 @@ export default function Cart() {
               <button
                 onClick={async () => {
                   setIsProcessing(true);
-                  const response = await makePayment();
-                  console.log(response);
+                  makePayment();
+                  fetchBalance();
                   setIsProcessing(false);
                 }}
                 type="button"
                 disabled={isProcessing}
-                className="text-white  text-lg text-center mt-5 w-56 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                className={`text-white ${isProcessing && "cursor-not-allowed"} text-lg text-center mt-5 w-56 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`}
               >
                 {isProcessing
                   ? "Processing..."
